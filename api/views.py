0e3cfc6b932cf48f3a6e7ac2c8dff4281fc88914
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-26 12:28:44 +0700

    feat: Реализовать эндпоинты категорий и продуктов

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from store.models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryListView(generics.ListAPIView):
+    """Эндпоинт для просмотра списка категорий с подкатегориями."""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+
+
+class ProductListView(generics.ListAPIView):
+    """Эндпоинт для просмотра списка продуктов."""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
