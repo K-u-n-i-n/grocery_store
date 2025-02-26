12325ffeec875b60ed3906a0f9c5f7290405a64b
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-26 11:32:47 +0700

    fix: Исправить метод автосохранения slug

--- store/models.py
+++ store/models.py
@@ -6,20 +6,21 @@ from django.core.files.base import ContentFile
 from django.db import models
 from django.utils.text import slugify
 from PIL import Image
+from unidecode import unidecode
 
 
 class CustomUser(AbstractUser):
-    '''Модель пользователя, наследуемая от AbstractUser.'''
+    """Модель пользователя, наследуемая от AbstractUser."""
 
     def __str__(self):
         return self.username
 
 
 class Category(models.Model):
-    '''Модель категории товаров.'''
+    """Модель категории товаров."""
 
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     image = models.ImageField(upload_to='categories/')
 
     class Meta:
@@ -30,43 +31,60 @@ class Category(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
+        """Переопределенный метод сохранения для авто-генерации slug."""
+
+        if (
+            not self.slug
+            or self.name != Category.objects.filter(id=self.id)
+            .values_list('name', flat=True)
+            .first()
+        ):
+            self.slug = slugify(unidecode(self.name))
         super().save(*args, **kwargs)
 
 
 class Subcategory(models.Model):
-    '''Модель подкатегории товаров, привязанная к категории.'''
+    """Модель подкатегории товаров, привязанная к категории."""
 
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE
     )
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True)
     image = models.ImageField(upload_to='subcategories/')
 
     class Meta:
         verbose_name = 'подкатегория'
         verbose_name_plural = 'Подкатегории'
         default_related_name = 'subcategories'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('category', 'slug'),
+                name='unique_subcategory_slug'
+            )
+        ]
 
     def __str__(self):
         return f'{self.category.name} - {self.name}'
 
     def save(self, *args, **kwargs):
+        """Переопределенный метод сохранения для авто-генерации slug."""
+
         if not self.slug:
-            self.slug = slugify(self.name)
+            if not self.category.slug:
+                self.category.save()
+            self.slug = f'{self.category.slug}-{slugify(unidecode(self.name))}'
         super().save(*args, **kwargs)
 
 
 class Product(models.Model):
-    '''Модель продукта с изображениями в 3-х размерах.'''
+    """Модель продукта с изображениями в 3-х размерах."""
 
     subcategory = models.ForeignKey(
         Subcategory, on_delete=models.CASCADE
     )
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_original = models.ImageField(
         upload_to='products/original/'
@@ -89,43 +107,52 @@ class Product(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        '''
-        Переопределенный метод сохранения для авто-генерации изображений
-        среднего размера и миниатюры.
-        '''
+        """
+        Переопределенный метод сохранения для авто-генерации slug
+        и создания изображений в 3-х размерах.
+        """
+
+        if not self.slug:
+            self.slug = slugify(unidecode(self.name))
         super().save(*args, **kwargs)
+
         if self.image_original and (
             not self.image_medium or not self.image_thumbnail
         ):
-            img = Image.open(self.image_original.path)
-            medium_size = (500, 500)
-            thumb_size = (100, 100)
+            try:
+                img = Image.open(self.image_original.path)
+                medium_size = (500, 500)
+                thumb_size = (100, 100)
 
-            # Создание среднего изображения
-            img_medium = img.copy()
-            img_medium.thumbnail(medium_size)
-            temp_medium = BytesIO()
-            img_medium.save(temp_medium, format=img.format)
-            temp_medium.seek(0)
-            medium_name = os.path.basename(self.image_original.name)
-            self.image_medium.save(medium_name, ContentFile(
-                temp_medium.read()), save=False)
+                # Создание среднего изображения
+                img_medium = img.copy()
+                img_medium.thumbnail(medium_size)
+                temp_medium = BytesIO()
+                img_medium.save(temp_medium, format=img.format)
+                temp_medium.seek(0)
+                medium_name = os.path.basename(self.image_original.name)
+                self.image_medium.save(medium_name, ContentFile(
+                    temp_medium.read()), save=False)
 
-            # Создание миниатюры
-            img_thumb = img.copy()
-            img_thumb.thumbnail(thumb_size)
-            temp_thumb = BytesIO()
-            img_thumb.save(temp_thumb, format=img.format)
-            temp_thumb.seek(0)
-            thumb_name = os.path.basename(self.image_original.name)
-            self.image_thumbnail.save(
-                thumb_name, ContentFile(temp_thumb.read()), save=False)
+                # Создание миниатюры
+                img_thumb = img.copy()
+                img_thumb.thumbnail(thumb_size)
+                temp_thumb = BytesIO()
+                img_thumb.save(temp_thumb, format=img.format)
+                temp_thumb.seek(0)
+                thumb_name = os.path.basename(self.image_original.name)
+                self.image_thumbnail.save(
+                    thumb_name, ContentFile(temp_thumb.read()), save=False)
 
-            super().save(update_fields=['image_medium', 'image_thumbnail'])
+                super().save(update_fields=['image_medium', 'image_thumbnail'])
+            except Exception as e:
+                raise RuntimeError(
+                    f'Ошибка при обработке изображений для {self.pk}: {e}'
+                )
 
 
 class Cart(models.Model):
-    '''Модель корзины, привязанная к пользователю.'''
+    """Модель корзины, привязанная к пользователю."""
 
     user = models.OneToOneField(
         CustomUser, on_delete=models.CASCADE
